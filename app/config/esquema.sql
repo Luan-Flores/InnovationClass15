d89c3195c4170525bd7cc7020288a311ed570af0
luan <keeganluan919@gmail.com>
2025-09-17 11:22:01 -0400

    adicionando campos na tabela PRODUTOS

--- app/config/esquema.sql
+++ app/config/esquema.sql
@@ -11,5 +11,8 @@ CREATE TABLE produtos (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   quantidade INT NOT NULL,
-  preco DECIMAL(10,2) NOT NULL
+  preco DECIMAL(10,2) NOT NULL,
+  sku VARCHAR(100) NOT NULL,
+  fornecedor VARCHAR(100) NOT NULL,
+  descricao VARCHAR(200) NOT NULL
 );
