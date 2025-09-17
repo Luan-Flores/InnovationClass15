61c0513c6eb295c1d45155edfe80aa726384fc2d
luan <keeganluan919@gmail.com>
2025-09-17 11:16:42 -0400

    header responsivo finalizado

---
+++ app/views/estoque.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LFStock</title>
+    <link rel="stylesheet" href="../../public/css/estoque.css">
+</head>
+<body>
+    <header>
+        <div class="hdr1">
+            <img src="../../public/imagens/stockLogo.png" alt="Logo">
+            <h2>LFStock Gerenciamento de Estoque</h2>
+        </div>
+        <div class="hdr2">
+            <div class="connect">
+                <img src="../../public/imagens/connected.png" alt="Conectado">
+                <p>Conectado</p>
+            </div>
+            <div class="logout">
+                <img src="../../public/imagens/logout.png" alt="Conectado">
+                <p>Sair</p>
+            </div>
+
+        </div>
+    </header>
+</body>
+</html>
